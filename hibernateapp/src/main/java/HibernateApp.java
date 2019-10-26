68cc6927259d223520aa6f62371253043c1bfe93
kostyawhite <kostyawhite89@gmail.com>
2019-10-26 16:54:57 +0300

    use jpa example

--- hibernateapp/src/main/java/HibernateApp.java
+++ hibernateapp/src/main/java/HibernateApp.java
@@ -1,15 +1,31 @@
 import services.UserService;
 import models.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Date;
+
 public class HibernateApp {
 
     public static void main(String[] args) {
+
+//        Java Persistence
+//        EntityManagerFactory entityManagerFactory;
+//        entityManagerFactory = Persistence.createEntityManagerFactory( "org.hibernate.tutorial.jpa" );
+//
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.getTransaction().begin();
+//        entityManager.persist( new User("John"));
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
 
         UserService userService = new UserService();
 
         User user = new User("Kostya");
         userService.saveUser(user);
         userService.findUser(1);
+        userService.saveUser(new User("Kseniya"));
 
     }
 
