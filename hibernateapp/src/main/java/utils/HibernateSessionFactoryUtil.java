5aba3537494305f0ba2c8ff036df9776b32ba976
kostyawhite <kostyawhite89@gmail.com>
2019-10-26 16:15:05 +0300

    check if sessionfactory was created

--- hibernateapp/src/main/java/utils/HibernateSessionFactoryUtil.java
+++ hibernateapp/src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -17,12 +17,15 @@ public class HibernateSessionFactoryUtil {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure()
                 .build();
-        try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        } catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
+
+        if (sessionFactory == null) {
+            try {
+                sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            } catch (Exception e) {
+                // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+                // so destroy it manually.
+                StandardServiceRegistryBuilder.destroy(registry);
+            }
         }
 
         return sessionFactory;
